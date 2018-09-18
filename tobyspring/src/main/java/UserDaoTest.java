2265fc225dcd2e9c42d0c238074ea93fcce47801
오세진 <jinioh88@gmail.com>
2018-09-18 13:07:59 +0900

    싱글톤 레즈스트리와 오브젝트 스코프

--- tobyspring/src/main/java/UserDaoTest.java
+++ tobyspring/src/main/java/UserDaoTest.java
@@ -1,7 +1,5 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import user.dao.ConnectionMaker;
-import user.dao.DConnectionMaker;
 import user.dao.DaoFactory;
 import user.dao.UserDao;
 import user.domain.User;
@@ -13,21 +11,24 @@ public class UserDaoTest {
         ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
 
         UserDao dao = context.getBean("userDao",UserDao.class);
+        UserDao dao2 = context.getBean("userDao",UserDao.class);
 
-        User user = new User();
-        user.setId("whiteship7");
-        user.setName("백기선");
-        user.setPasswor("married");
+//        User user = new User();
+//        user.setId("whiteship7");
+//        user.setName("백기선");
+//        user.setPasswor("married");
+//
+//        dao.add(user);
 
-        dao.add(user);
-
-        System.out.println(user.getId()+ " 등록 성공");
-
-        User user2 = dao.get(user.getId());
-        System.out.println(user2.getName());
-        System.out.println(user2.getPasswor());
-
-        System.out.println(user2.getId()+" 조회 성공!");
+//        System.out.println(user.getId()+ " 등록 성공");
+//
+//        User user2 = dao.get(user.getId());
+//        System.out.println(user2.getName());
+//        System.out.println(user2.getPasswor());
+//
+//        System.out.println(user2.getId()+" 조회 성공!");
+        System.out.println(dao);
+        System.out.println(dao2);
 
     }
 }
