28ea8823a112cb6994bd8f76b548e66a4d8c9588
오세진 <jinioh88@gmail.com>
2018-09-20 13:44:39 +0900

    템플릿 : 분리와 재사용

--- tobyspring/src/main/java/user/dao/UserDao.java
+++ tobyspring/src/main/java/user/dao/UserDao.java
@@ -1,13 +1,12 @@
 package user.dao;
 
 import org.springframework.dao.EmptyResultDataAccessException;
-import sun.tools.tree.ConditionalExpression;
 import user.domain.User;
 
 import javax.sql.DataSource;
 import java.sql.*;
 
-public class UserDao {
+public abstract class UserDao {
     private DataSource dataSource;
 
     public void setDataSource(DataSource dataSource) {
@@ -51,30 +50,68 @@ public class UserDao {
         return user;
     }
 
-    public void deleteAll() throws SQLException {
-        Connection c = dataSource.getConnection();
-        PreparedStatement ps = c.prepareStatement("delete from users");
-        ps.executeUpdate();
+    public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
+        Connection c = null;
+        PreparedStatement ps = null;
+        try {
+            c = dataSource.getConnection();
 
-        ps.close();
-        c.close();
+            ps = stmt.makePreparedStatement(c);
+            ps.executeUpdate();
+        } catch(SQLException e) {
+            throw e;
+        } finally {
+            if(ps!=null) {
+                try {
+                    ps.close();
+                } catch(SQLException e){}
+            }
+            if(c!=null) {
+                try{
+                    c.close();
+                }catch(SQLException e) {}
+            }
+        }
+    }
+
+    public void deleteAll() throws SQLException {
+        StatementStrategy st = new DeleteAllStatement();
+        jdbcContextWithStatementStrategy(st);
     }
 
     public int getCount()  throws SQLException {
-        Connection c = dataSource.getConnection();
-        PreparedStatement ps = c.prepareStatement("select count(*) from users");
+        Connection c = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
 
-        ResultSet rs = ps.executeQuery();
-        rs.next();
-        int count = rs.getInt(1);
-
-        rs.close();
-        ps.close();
-        c.close();
-
-        return count;
+        try {
+            c = dataSource.getConnection();
+            ps = c.prepareStatement("select count(*) from users");
+            rs = ps.executeQuery();
+            rs.next();
+            return rs.getInt(1);
+        } catch(SQLException e) {
+            throw e;
+        } finally {
+            if(rs!=null) {
+                try {
+                    rs.close();
+                } catch(SQLException e){}
+            }
+            if(ps!=null) {
+                try {
+                    ps.close();
+                } catch(SQLException e){}
+            }
+            if(c!=null) {
+                try{
+                    c.close();
+                }catch(SQLException e) {}
+            }
+        }
     }
 
+    abstract protected PreparedStatement makeStatement(Connection c) throws SQLException;
 }
 
 
---
+++ tobyspring/src/main/java/user/dao/UserDaoDeleteall.java
@@ -0,0 +1,14 @@
+package user.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class UserDaoDeleteall extends UserDao {
+
+    @Override
+    protected PreparedStatement makeStatement(Connection c) throws SQLException {
+        PreparedStatement ps = c.prepareStatement("delete from users");
+        return ps;
+    }
+}
