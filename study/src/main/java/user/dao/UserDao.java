8f8b74dd720e94746b6781d607c1d5882392cc05
ohsejin <jinioh88@gmail.com>
2018-10-15 13:57:27 +0900

    메서드의 분리

--- study/src/main/java/user/dao/UserDao.java
+++ study/src/main/java/user/dao/UserDao.java
@@ -6,8 +6,8 @@ import java.sql.*;
 
 public class UserDao {
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook","spring","book");
+
+        Connection c = getConnection();
         PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) value (?,?,?)");
         ps.setString(1,user.getId());
         ps.setString(2,user.getName());
@@ -20,8 +20,7 @@ public class UserDao {
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook","spring","book");
+        Connection c = getConnection();
         PreparedStatement ps = c.prepareStatement("select * from users where id =?");
         ps.setString(1,id);
 
@@ -37,5 +36,28 @@ public class UserDao {
         c.close();
 
         return user;
+    }
+
+    private Connection getConnection()  throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook","spring","book");
+        return c;
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        UserDao dao = new UserDao();
+        User user = new User();
+        user.setId("whiteship2");
+        user.setName("백기선");
+        user.setPassword("married");
+
+        dao.add(user);
+
+        System.out.println(user.getId()+" 등록 성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+
+        System.out.println(user2.getId()+ " 조회 성공");
     }
 }
