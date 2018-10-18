74276c70474cb3bfb6d78f587ac49499252b4e2f
ohsejin <jinioh88@gmail.com>
2018-10-18 15:28:37 +0900

    학습테스트

--- study/src/main/java/user/dao/DaoMain.java
+++ study/src/main/java/user/dao/DaoMain.java
@@ -3,6 +3,6 @@ package user.dao;
 import org.junit.runner.JUnitCore;
 public class DaoMain {
     public static void main(String[] args) {
-        JUnitCore.main("user.dao.UserDaoTest");
+        JUnitCore.main("user.UserDaoTest");
     }
 }
--- study/src/test/java/user/UserDaoTest.java
+++ study/src/test/java/user/UserDaoTest.java
@@ -1,4 +1,4 @@
-package user.dao;
+package user;
 
 
 import org.junit.Before;
@@ -6,15 +6,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.datasource.SingleConnectionDataSource;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import user.dao.UserDao;
 import user.domain.User;
 
-import javax.sql.DataSource;
 import java.sql.SQLException;
 
 import static org.hamcrest.core.Is.is;
