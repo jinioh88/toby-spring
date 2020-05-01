cb6d2ca9a2c89bac2f3dfd75157fd605b5580c2c
jinioh88 <jinioh88@gmail.com>
2020-05-01 13:52:43 +0900

    feat: 토비스프링부트

---
+++ toby-boot/src/main/java/study/tobyboot/TobyBootApplication.java
@@ -0,0 +1,13 @@
+package study.tobyboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobyBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TobyBootApplication.class, args);
+    }
+
+}
